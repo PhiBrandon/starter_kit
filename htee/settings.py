139af365e75d5341f311296e94c545cd6e784526
Brandon Phillips <phillipsbrandonit@gmail.com>
2024-05-24 05:29:00 -0600

    environment

--- htee/settings.py
+++ htee/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&z^!&t!=otrl$@$65&ey#hp*!+l&*&9fk+jrc!r*ni09^+f-8q"
+SECRET_KEY = os.environ["DJANGO_SECRET"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "bpdata.io"]
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
