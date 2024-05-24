260f23c7fd4be5ffb29eb365925080f9f5f4ba63
Brandon Phillips <phillipsbrandonit@gmail.com>
2024-05-24 05:55:28 -0600

    collected static

--- htee/settings.py
+++ htee/settings.py
@@ -132,10 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/theme/static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / 'theme/static_src',
 ]
+STATIC_ROOT = BASE_DIR / "static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
